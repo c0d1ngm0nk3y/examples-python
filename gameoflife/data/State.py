7dc9f200be10806202397daf44436dff1668e865
Ralf Pannemans <ralf.pannemans@googlemail.com>
2017-11-11 18:53:22 +0100

    Add is_alive for state

--- gameoflife/data/State.py
+++ gameoflife/data/State.py
@@ -1,20 +1,37 @@
+from array import array
+
 class State(object):
     CELL_DEAD = "."
     CELL_ALIVE = "*"
 
+    CELL_DEAD_INT = 46
+    CELL_ALIVE_INT = 47
+
     def __init__(self, rows=0, columns=0):
         self.rows = rows
         self.columns = columns
-        self.field = "." * rows * columns
+        self.field = array('b', self.CELL_DEAD * rows * columns)
 
-    def get_cell(self, x_coord, y_coord):
+    def _get_cell(self, x_coord, y_coord):
         if not self._check_coordinates(x_coord, y_coord):
             return None
         i = x_coord * self.columns + y_coord
         return self.field[i]
+
+    def is_dead(self, x_ccord, y_coord):
+        value = self._get_cell(x_ccord, y_coord)
+        return value == self.CELL_DEAD_INT
+
+    def is_alive(self, x_ccord, y_coord):
+        value = self._get_cell(x_ccord, y_coord)
+        return value == self.CELL_ALIVE_INT
 
     def _check_coordinates(self, x_coord, y_coord):
         return (x_coord < self.rows) and (y_coord < self.columns)
 
     def get_dimensions(self):
         return (self.rows, self.columns)
+
+    def set_alive(self, x_coord, y_coord):
+        i = x_coord * self.columns + y_coord
+        self.field[i] = self.CELL_ALIVE_INT
--- gameoflife/data/StateTest.py
+++ gameoflife/data/StateTest.py
@@ -6,19 +6,11 @@ from gameoflife.data.State import State
 class StateTest(unittest.TestCase):
     def test_empty_state_begin(self):
         state = State(2, 2)
-        self.assertEqual(State.CELL_DEAD, state.get_cell(0, 0))
+        self.assertTrue(state.is_dead(0, 0))
 
     def test_empty_state_end(self):
         state = State(3, 3)
-        self.assertEqual(State.CELL_DEAD, state.get_cell(2, 2))
-
-    def test_invalid_row(self):
-        state = State(3, 3)
-        self.assertEqual(None, state.get_cell(3, 0))
-
-    def test_invalid_column(self):
-        state = State(3, 3)
-        self.assertEqual(None, state.get_cell(0, 3))
+        self.assertTrue(state.is_dead(2, 2))
 
     def test_get_dimensions_2_3(self):
         state = State(2, 3)
@@ -27,6 +19,26 @@ class StateTest(unittest.TestCase):
     def test_get_dimensions_5_1(self):
         state = State(5, 1)
         self.assertEqual((5, 1), state.get_dimensions())
+
+    def test_set_alive_1_3(self):
+        state = State(5, 5)
+        state.set_alive(1, 3)
+        self.assertTrue(state.is_alive(1, 3))
+
+    def test_set_alive_2_4(self):
+        state = State(5, 5)
+        state.set_alive(2, 4)
+        self.assertTrue(state.is_alive(2, 4))
+
+    def test_invalid_row(self):
+        state = State(5, 5)
+        self.assertFalse(state.is_alive(5, 1))
+        self.assertFalse(state.is_dead(5, 1))
+
+    def test_invalid_column(self):
+        state = State(5, 5)
+        self.assertFalse(state.is_alive(1, 5))
+        self.assertFalse(state.is_dead(1, 5))
 
 
 if __name__ == '__main__':
