bb47c8232765fc4970bc44a7b79588e204724a67
Ralf Pannemans <ralf.pannemans@googlemail.com>
2017-11-11 22:49:34 +0100

    Add game of life logic

--- gameoflife/Game.py
+++ gameoflife/Game.py
@@ -3,4 +3,17 @@ from gameoflife.data.State import State
 def next_generation(state):
     (rows, columns) = state.get_dimensions()
     next_state = State(rows, columns)
+
+    for row in xrange(0, rows):
+        for column in xrange(0, columns):
+            count = state.count_alive_neighbours(row, column)
+            if count < 2:
+                pass
+            elif count == 2 or count == 3:
+                next_state.set_alive(row, column)
+            elif state.is_alive(row, column):
+                pass
+            else:
+                next_state.set_alive(row, column)
+
     return next_state
--- gameoflife/GameTest.py
+++ gameoflife/GameTest.py
@@ -4,11 +4,41 @@ from gameoflife.Game import next_generation
 from gameoflife.data.StateReader import from_string
 
 class GameTest(unittest.TestCase):
-    def test_dying(self):
+    def test_dying_alone(self):
         state = from_string(1, 3, ".*.")
         next_state = next_generation(state)
 
         self.assertFalse(next_state.is_alive(0, 1))
+
+    def test_not_dying_2(self):
+        state = from_string(1, 3, "***")
+        next_state = next_generation(state)
+
+        self.assertTrue(next_state.is_alive(0, 1))
+
+    def test_not_dying_3(self):
+        state = from_string(2, 3, "***.*.")
+        next_state = next_generation(state)
+
+        self.assertTrue(next_state.is_alive(0, 1))
+
+    def test_not_dying_3_variation(self):
+        state = from_string(2, 3, ".*.***")
+        next_state = next_generation(state)
+
+        self.assertTrue(next_state.is_alive(1, 1))
+
+    def test_dying_5(self):
+        state = from_string(2, 3, "******")
+        next_state = next_generation(state)
+
+        self.assertFalse(next_state.is_alive(1, 1))
+
+    def test_ressurrect_4(self):
+        state = from_string(2, 3, "*.**.*")
+        next_state = next_generation(state)
+
+        self.assertTrue(next_state.is_alive(1, 1))
 
 
 if __name__ == '__main__':
